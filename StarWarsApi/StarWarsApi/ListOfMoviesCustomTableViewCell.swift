49aab7dfed5598c761c1ff55950923279a895dfb
Anton Huisamen <antonmcde@icloud.com>
2018-09-04 12:38:19 +0200

    ViewControllers and CustomCell Xib

---
+++ StarWarsApi/StarWarsApi/ListOfMoviesCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListOfMoviesCustomTableViewCell.swift
+//  StarWarsApi
+//
+//  Created by Anton Huisamen on 2018/09/04.
+//  Copyright © 2018 antonsolo. All rights reserved.
+//
+
+import UIKit
+
+class ListOfMoviesCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
